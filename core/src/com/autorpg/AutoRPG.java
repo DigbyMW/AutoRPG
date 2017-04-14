f1569d9d801e6d835bedd791ac8836619d5995cb
Digby White <digbywhite@gmail.com>
2017-04-14 20:14:43 +0100

    Testing for TileSystem

--- core/src/com/autorpg/AutoRPG.java
+++ core/src/com/autorpg/AutoRPG.java
@@ -1,12 +1,15 @@
 package com.autorpg;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.ApplicationAdapter; // Main class
+import com.badlogic.gdx.Gdx; // Used for clearing the screen with opengl
+import com.badlogic.gdx.graphics.GL20; // Used for getting colours
+import com.badlogic.gdx.graphics.g2d.TextureRegion; // Used for tiles.
 
 public class AutoRPG extends ApplicationAdapter {
-	TextureRegion region;
+	private TextureRegion region;
+	private TextureRegion test_r;
+	private TextureRegion test_g;
+	private TextureRegion test_b;
 	private TileSystem tile_system;
 	
 	@Override
@@ -14,6 +17,9 @@ public class AutoRPG extends ApplicationAdapter {
 		// Initializes tile system and region.
 		tile_system = new TileSystem();
 		region = tile_system.get_tile("placeholder");
+		test_r = tile_system.get_tile("test r");
+		test_g = tile_system.get_tile("test g");
+		test_b = tile_system.get_tile("test b");
 	}
 
 	@Override
@@ -25,6 +31,10 @@ public class AutoRPG extends ApplicationAdapter {
 		// Draw tile at 1, 1
 		tile_system.batch_begin();
 		tile_system.draw_tile(region, 1, 1);
+		tile_system.draw_tile(region, 2, 1);
+		tile_system.draw_tile(test_r, 3, 1);
+		tile_system.draw_tile(test_g, 4, 2);
+		tile_system.draw_tile(test_b, 5, 3);
 		tile_system.batch_end();
 	}
 	
